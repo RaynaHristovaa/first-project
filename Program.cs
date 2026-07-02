2825901be933d4604cf7aec2124a1db350ac148b
Rayna <rayna_hristova82@abv.bg>
2026-07-02 11:55:58 +0300

    Create Menu

--- Program.cs
+++ Program.cs
@@ -4,9 +4,24 @@
     {
         static void Main(string[] args)
         {
-       
 
 
+            List<person> people = LoadPeopleFromFile();
+            bool running = true;    
+
+            while(running)
+            {
+                Console.WriteLine("----Избери операция----");
+                Console.WriteLine("1. Create (Добавяне на човек)");
+                Console.WriteLine("2. READ (Показване на всички)");
+                Console.WriteLine("3.UPDATE (Промяна на заплата)");
+                Console.WriteLine("3.UPDATE (Промяна на заплата)");
+                Console.WriteLine("4.DELETE (Изтриване по име)");
+                Console.WriteLine("5.Изход ");
+                Console.WriteLine("Избор: ");
+                string choice = Console.ReadLine();
+              
+            }
 
 
 
